f0d2d12a908fdd6e6c8cdb7049a78d3110ac8251
Susmit Kumar Pandey <susmitpandey22@gmail.com>
2024-09-29 00:58:56 +0530

    Integrated the gemini API for better responses

--- Voice Assistant/main.py
+++ Voice Assistant/main.py
@@ -4,8 +4,19 @@ from sel import infow
 import subprocess
 from weather import Weather
 from tkinter import *
+from dotenv import load_dotenv
 import sys
 import threading
+import google.generativeai as genai
+import os
+import re
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY_GEMINI")
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat()
 
 
 engine = p.init()
@@ -25,8 +36,27 @@ canvas = Canvas(width=400, height=400)
 t_image = PhotoImage(file="V_pic.png")
 canvas.create_image(200, 150, image=t_image)
 action_text = canvas.create_text(
-    200, 300, text="Welcome...", font=("Courier", 15, "bold"))
+    200, 330, text="Welcome...", font=("Courier", 15, "bold"), width=300)
 canvas.grid(row=1, column=1)
+
+
+# For handling the emojis if they appear in the response.
+emoji_pattern = re.compile(
+    "["
+    "\U0001F600-\U0001F64F"  # Emoticons
+    "\U0001F300-\U0001F5FF"  # Miscellaneous Symbols and Pictographs
+    "\U0001F680-\U0001F6FF"  # Transport and Map Symbols
+    "\U0001F700-\U0001F77F"  # Alchemical Symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "\U000024C2-\U0001F251"  # Enclosed Characters
+    "]+", flags=re.UNICODE
+)
+
 
 listening_thread = None
 
@@ -60,36 +90,36 @@ def greeting():
     speak("Hello Sir. I am Cassie. How May I help you.")
 
 
-def handle_command(text):
-    if "search" and "wikipedia" in text.lower():
+def handle_command(tex):
+    if "search" and "wikipedia" in tex.lower():
         speak("Please tell me the topic sir")
         query = listen()
         assist = infow()
         assist.get_info(query)
 
-    elif "video" and "youtube" in text.lower():
+    elif "video" and "youtube" in tex.lower():
         speak("Please tell me the name of the song you want to play")
         query = listen()
         speak("Enjoy the song")
         assist = infow()
         assist.play_video(query)
 
-    elif "open" and "excel" in text.lower():
+    elif "open" and "excel" in tex.lower():
         speak("Opening Excel")
         subprocess.Popen(
             "C:/Program Files/Microsoft Office/root/Office16/EXCEL.EXE")
 
-    elif "open" and "word" in text.lower():
+    elif "open" and "word" in tex.lower():
         speak("Opening Word")
         subprocess.Popen(
             "C:/Program Files/Microsoft Office/root/Office16/WINWORD.EXE")
 
-    elif "open" and "powerpoint" in text.lower():
+    elif "open" and "powerpoint" in tex.lower():
         speak("Opening PowerPoint")
         subprocess.Popen(
             "C:/Program Files/Microsoft Office/root/Office16/POWERPNT.EXE")
 
-    elif "weather" in text.lower():
+    elif "weather" in tex.lower():
         speak("Providing weather details")
         w = Weather()
         data = w.get_weather()
@@ -98,7 +128,7 @@ def handle_command(text):
             f"Temperature feels like {data['main']['feels_like']} Fahrenheit")
         speak(f"Wind speed is {data['wind']['speed']} km per hour")
 
-    elif "exit" in text.lower():
+    elif "exit" in tex.lower():
         try:
             speak("Have a good day")
             screen.quit()  # Stop the Tkinter main loop
@@ -107,8 +137,10 @@ def handle_command(text):
         except RuntimeError:
             pass
 
-    elif "how" and "are" and "you" in text.lower():
-        speak("I am fine sir.")
+    else:
+        response = chat.send_message(tex).text[:400].replace('*', '')
+        response = emoji_pattern.sub(r'', response)
+        speak(response)
 
 
 def update_text():
